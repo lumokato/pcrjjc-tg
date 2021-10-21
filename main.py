ae7eda107280192addeb76ba6054da08a5414da4
lumokato <suzto@qq.com>
2021-10-21 13:58:09 +0800

    bind

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from telegram.ext import Updater, CommandHandler, JobQueue
+from telegram.ext import Updater, CommandHandler
 import config as cg
-import time
 import logging
-from telegram.ext.dispatcher import run_async
-import new_jjc as jjc
+import jjc
 
 root = logging.getLogger()
 root.setLevel(logging.INFO)
@@ -20,9 +18,15 @@ logger = logging.getLogger(__name__)
 def main():
     bot = Updater(token=cg.TOKEN, request_kwargs={'proxy_url': cg.proxy_url}, use_context=True)
     dp = bot.dispatcher
-    dp.add_handler(CommandHandler('start', jjc.start, run_async=True))
+    dp.add_handler(CommandHandler('query', jjc.on_query_arena, run_async=True))
+    dp.add_handler(CommandHandler('queryall', jjc.on_query_arena_all, run_async=True))
     dp.add_handler(CommandHandler('help', jjc.send_jjchelp, run_async=True))
-    # dp.add_handler(JobQueue, start)
+    dp.add_handler(CommandHandler('bind', jjc.on_arena_bind, run_async=True))
+    dp.add_handler(CommandHandler('change', jjc.change_arena_sub, run_async=True))
+    dp.add_handler(CommandHandler('del', jjc.delete_arena_sub, run_async=True))
+    dp.add_handler(CommandHandler('status', jjc.send_arena_sub_status, run_async=True))
+    # dp.add_handler(CommandHandler('start', jjc.start_schedule, pass_job_queue=True))
+    bot.job_queue.run_repeating(jjc.on_arena_schedule, 60)
     bot.start_polling()
 
 
