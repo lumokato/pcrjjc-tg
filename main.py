6218edbf2f674a6bc348be65daacb3438b6f4525
lumokato <suzto@qq.com>
2022-01-18 20:07:46 +0800

    top

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import config as cg
 import logging
 import jjc
 import infedg as clan
+import top
 
 root = logging.getLogger()
 root.setLevel(logging.INFO)
@@ -28,6 +29,7 @@ def main():
     dp.add_handler(CommandHandler('status', jjc.send_arena_sub_status, run_async=True))
     dp.add_handler(CommandHandler('rate', jjc.damage_percentage_stage, run_async=True))
     dp.add_handler(CommandHandler('refresh', clan.refresh_damage, run_async=True))
+    dp.add_handler(CommandHandler('top', top.on_query_top, run_async=True))
     # dp.add_handler(CommandHandler('start', jjc.start_schedule, pass_job_queue=True))
     bot.job_queue.run_repeating(jjc.on_arena_schedule, 30)
     bot.job_queue.run_repeating(jjc.damage_stage, 1800, first="2021-10-28 8:01:00", last="2021-12-30 23:01:00")
