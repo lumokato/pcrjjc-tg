c125ffb1fa3278f9019b46547b4828bf996a2038
lumokato <suzto@qq.com>
2022-05-15 18:13:49 +0800

    rm

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from telegram.ext import Updater, CommandHandler
 import config as cg
 import logging
 import jjc
-import infedg as clan
 import top
 
 root = logging.getLogger()
@@ -28,14 +27,12 @@ def main():
     dp.add_handler(CommandHandler('del', jjc.delete_arena_sub, run_async=True))
     dp.add_handler(CommandHandler('status', jjc.send_arena_sub_status, run_async=True))
     dp.add_handler(CommandHandler('rate', jjc.damage_percentage_stage, run_async=True))
-    dp.add_handler(CommandHandler('refresh', clan.refresh_damage, run_async=True))
     dp.add_handler(CommandHandler('aa', top.on_query_atop, run_async=True))
     dp.add_handler(CommandHandler('pp', top.on_query_ptop, run_async=True))
     dp.add_handler(CommandHandler('a', top.on_query_alist, run_async=True))
     dp.add_handler(CommandHandler('p', top.on_query_plist, run_async=True))
     # dp.add_handler(CommandHandler('start', jjc.start_schedule, pass_job_queue=True))
     bot.job_queue.run_repeating(jjc.on_arena_schedule, 30)
-    # bot.job_queue.run_repeating(clanbattle.stage_data, 1800, first="2022-01-21 8:01:00", last="2022-02-27 23:51:00")
     bot.job_queue.run_repeating(top.on_query_ptop, 86400, first="2022-01-18 14:40:00", last="2022-12-30 23:01:00")
     bot.start_polling()
 
