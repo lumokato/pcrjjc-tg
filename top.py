43981c68e176ae9d6ebe87fffdcf79fc1a24e05d
lumokato <suzto@qq.com>
2022-10-07 18:39:28 +0800

    main

--- top.py
+++ top.py
@@ -139,7 +139,7 @@ def on_query_pwild():
         write_flag = 0
         if 'ranking' in res:
             for user in res['ranking']:
-                if str(user['viewer_id']) not in group_user["group"] and user['rank'] < 7:
+                if str(user['viewer_id']) not in group_user["group"] and user['rank'] < 8:
                     wild_flag = 1
                     if str(user['viewer_id']) not in group_user["wild"]:
                         write_flag = 1
