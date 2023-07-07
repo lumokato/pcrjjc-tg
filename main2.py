ae398259d53a1c7ffc56eb6b170e24b1e2eaf2c9
lumokato <suzto@qq.com>
2023-07-07 16:06:36 +0800

    main

--- main2.py
+++ main2.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater, CommandHandler
-from apscheduler.schedulers.background import BlockingScheduler
+# from apscheduler.schedulers.background import BlockingScheduler
 from json import load
 import top
 import hide
@@ -13,7 +13,7 @@ def main():
     dp = bot.dispatcher
     dp.add_handler(CommandHandler('pp', top.on_query_ptop, run_async=True))
     dp.add_handler(CommandHandler('p', top.on_query_plist, run_async=True))
-    dp.add_handler(CommandHandler('hide', hide.hide_process, run_async=True))
+    dp.add_handler(CommandHandler('hide', hide.hide_process, run_async=True), args=["2"])
     bot.start_polling()
 
 
@@ -23,9 +23,9 @@ def refresh_daily(scheduler):
 
 
 if __name__ == '__main__':
-    # main()
-    scheduler = BlockingScheduler(timezone="Asia/Shanghai")
+    main()
+    # scheduler = BlockingScheduler(timezone="Asia/Shanghai")
     # scheduler.add_job(top.on_query_pwild, id='query', trigger='cron', second='30', max_instances=100)
-    scheduler.add_job(hide.hide_process, id='hide1', trigger='cron', hour='14', minute='52', second='45', args=["1"])
-    scheduler.add_job(hide.hide_process, id='hide2', trigger='cron', hour='14', minute='55', second='56', args=["2"])
-    scheduler.start()
+    # scheduler.add_job(hide.hide_process, id='hide1', trigger='cron', hour='14', minute='52', second='45', args=["1"])
+    # scheduler.add_job(hide.hide_process, id='hide2', trigger='cron', hour='14', minute='55', second='56', args=["2"])
+    # scheduler.start()
