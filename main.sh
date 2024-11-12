0c54c38418fb86581ea26488c2cf3f8001f86cf0
lumokato <suzto@qq.com>
2024-11-12 17:25:38 +0800

    fix

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-ps aux|grep bot.py|grep -v grep |awk '{print $2}'|xargs kill -9
+ps aux|grep bot1.py|grep -v grep |awk '{print $2}'|xargs kill -9
 ps aux|grep bot2.py|grep -v grep |awk '{print $2}'|xargs kill -9
-nohup python3 bot.py &
+nohup python3 bot1.py &
 nohup python3 bot2.py &
